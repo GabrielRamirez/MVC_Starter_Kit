38a89c37adc40571e22a94a687a55c59e942b86c
unknown <107680@WPD-M67890.ent.co.ventura.ca.us>
2015-04-02 17:01:19 -0700

    Basic HTML webpage with Bootstrap and jQuery support

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title></title>
+		<!-- Bootstrap -->
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<!-- Content -->
+		</div>
+	</body>
+</html>
